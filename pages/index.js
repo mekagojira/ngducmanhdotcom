a8583cd6598b74858a044b6cb76c978ad6366d0f
Manh Nguyen Duc <manh@ngducmanh.com>
2020-10-03 18:31:09 +0700

    update

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="h-screen flex justify-center items-center">
-      <img src="/IMG.jpeg" className="h-full w-auto" />
+      <img src="/IMG.jpeg" className="w-1/2" />
     </div>
   );
 }
